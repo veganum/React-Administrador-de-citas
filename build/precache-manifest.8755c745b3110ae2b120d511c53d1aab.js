48c88ce7e8a6d910c3c08e4f1003e76b864f9f19
José <veganum@hotmail.com>
2020-02-10 22:06:26 +0100

    Primer commit

---
+++ build/precache-manifest.8755c745b3110ae2b120d511c53d1aab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50d82c8f955f46f3220be62f83d7bf50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d215864e36e38c6e1f1c",
+    "url": "/static/css/main.4c798faf.chunk.css"
+  },
+  {
+    "revision": "826bb55a7c72d8f1c05d",
+    "url": "/static/js/2.4146f786.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.4146f786.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d215864e36e38c6e1f1c",
+    "url": "/static/js/main.46616d61.chunk.js"
+  },
+  {
+    "revision": "cde57d688a04a82c7120",
+    "url": "/static/js/runtime-main.8d5f7f53.js"
+  }
+]);
